a8b7a4f135638c15b3b0f45c57ec3f7589cf5767
Iaconi Gheorghe <iaconimgheorghe@gmail.com>
2021-06-19 18:41:22 +0200

    first commit, project built with create-react-app, bootstrap, bootstrap icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,15 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Edit src/App.js and save to reload.</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="https://reactjs.org" className="text-decoration-none mr-2">
+          Example link text
+          <i className="bi bi-arrow-right-square ml-2"></i>
         </a>
       </header>
     </div>
